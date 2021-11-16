fcb95ff31696b50dbb688b1095710e4b09b47713
mayankmahi <mkmahi1198@gmail.com>
2021-11-16 19:27:56 +0530

    api created

--- index.js
+++ index.js
@@ -7,12 +7,82 @@ const app = express()
 // create port
 const PORT = process.env.PORT || 5000
 
+const generateScrapperUrl = apiKey =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
 // allow application to parse JSON input
 app.use(express.json())
 
 // Create routes
 app.get('/', (req, res) => {
   res.send('Welcome to Amozon Scrapper API')
+})
+
+// Get product details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params
+  const { api_key } = req.query
+  try {
+    const response = await request(
+      `${generateScrapperUrl(
+        api_key
+      )}&url=https://www.amazon.com/dp/${productId}`
+    )
+    res.json(JSON.parse(response))
+    console.log(response)
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+// get products reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+  const { productId } = req.params
+  const { api_key } = req.query
+
+  try {
+    const response = await request(
+      `${generateScrapperUrl(
+        api_key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
+})
+// get products offers
+app.get('/products/:productId/offers', async (req, res) => {
+  const { productId } = req.params
+  const { api_key } = req.query
+
+  try {
+    const response = await request(
+      `${generateScrapperUrl(
+        api_key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+// get search Results
+app.get('/search/:searchQuery', async (req, res) => {
+  const { searchQuery } = req.params
+  const { api_key } = req.query
+
+  try {
+    const response = await request(
+      `${generateScrapperUrl(
+        api_key
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
 })
 
 // Start the server
